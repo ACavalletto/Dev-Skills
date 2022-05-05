1bc842b58a3b293b930099df2438ec56151d39fb
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-04 18:10:26 -0600

    Added button functionality w/ jQuery & JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-let $btn = $("button"); 
+$("button").click(function () {
+    let $input = $("#input-area").val()
+    let $list = $("#list");
+    let newListItem = document.createElement('li');
+    newListItem.innerHTML = `<i class="fa-solid fa-circle-xmark"></i> ${$input}`;
+    $list.append(newListItem);
+})
+
+$("i").click(function (e) {
+    let target = e.target;
+    target.parentElement.remove();
+
+})
