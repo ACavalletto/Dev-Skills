bb1cd71b810a32cf321ad473dd645ec37485b466
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-04 20:16:04 -0600

    Messed around with animation in CSS.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $("button").click(function () {
-    let $input = $("#input-area").val()
+  let $input = $("#input-area").val();
+  if ($input !== "") {
     let $list = $("#list");
-    let newListItem = document.createElement('li');
+    let newListItem = document.createElement("li");
     newListItem.innerHTML = `</a><i class="fa-solid fa-circle-xmark fa-lg"></i> ${$input}`;
     $list.append(newListItem);
-})
+  }
+});
 
-let list = document.getElementById('skills-list');
+let list = document.getElementById("skills-list");
 
-list.addEventListener('click', (e) => {
-    let target = e.target;
-    if (target.tagName === "I") {
-        target.parentElement.remove();
-    }
-})
-
-
+list.addEventListener("click", (e) => {
+  let target = e.target;
+  if (target.tagName === "I") {
+    target.parentElement.remove();
+  }
+});
